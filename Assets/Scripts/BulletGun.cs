a7622fd02fa4b0383fc68c2c307d438dead4f964
MadEwink <ewokisamail@gmail.com>
2019-09-17 17:19:01 +0200

    when things move in harmony, the game is prettier
    adjustments on speed and stuffs

---
+++ Assets/Scripts/BulletGun.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGun : MonoBehaviour
+{
+    [SerializeField]
+    private SimpleBullet projectile;
+    private float timer;
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private Vector2 speed;
+    [SerializeField]
+    private float coolDown;
+
+    public float Damage {
+        get { return this.damage; }
+        private set { this.damage = value; }
+    }
+    public Vector2 Speed {
+        get { return this.speed; }
+        set { this.speed = value; }
+    }
+    public float CoolDown {
+        get { return this.coolDown; }
+        set { this.coolDown = value; }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if ( timer >= CoolDown )
+        {
+            var new_projectile = Instantiate(projectile, transform.position, transform.rotation);
+            new_projectile.Init(Damage, Speed, transform.position);
+            timer = 0f;
+        }
+    }
+}
