b6b80a290d7a3f79643d5f4314b1374d52dc0882
MadEwink <ewokisamail@gmail.com>
2019-09-17 16:19:03 +0200

    abstract bullets means abstract hole in your brain
    add enemy_bullet and player_bullet prefabs
    add abstract Bullet component
    	virtual init and update position functions
    	destroy on became invisible

--- Assets/Scripts/BaseAvatar.cs
+++ Assets/Scripts/BaseAvatar.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseAvatar : MonoBehaviour
+public abstract class BaseAvatar : MonoBehaviour
 {
     [SerializeField] private float maxSpeed;
 
@@ -10,17 +10,5 @@ public class BaseAvatar : MonoBehaviour
     {
         get { return this.maxSpeed; }
         private set { this.maxSpeed = value; }
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Bullet : MonoBehaviour
+{
+    private float damage;
+    private Vector2 speed;
+    private Vector2 position;
+    
+    public float Damage
+    {
+        get { return this.damage; }
+        set { this.damage = value; }
+    }
+    public float Speed
+    {
+        get { return this.speed }
+        set { this.speed = value; }
+    }
+    public float Position
+    {
+        get { return this.position }
+        private set { this.position = value; }
+    }
+
+    public virtual void Init(float damage, Vector2 speed, Vector2 position);
+    public virtual void UpdatePosition();
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
